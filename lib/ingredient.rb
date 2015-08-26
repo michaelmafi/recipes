c429482a00fa6b6e24e563444c5b0f684dc29a59
george denkey & Iman Mafi <student@epicodus.com>
2015-08-26 16:17:36 -0700

    libs and specs completed

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,3 +1,16 @@
 class Ingredient < ActiveRecord::Base
 
+  has_and_belongs_to_many(:recipes)
+  validates(:name, :uniqueness => true)
+
+  define_singleton_method(:find_name) do |name|
+
+    found_ingredient = nil
+    Ingredient.all().each()do |ingredient|
+    if ingredient.name().==(name)
+      found_ingredient = ingredient
+    end
+    found_ingredient
+  end
+ end
 end
